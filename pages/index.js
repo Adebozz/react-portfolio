c0627ba16945dd2e57ccb7aec32ef1bd005ac46f
Adebozz <ademolaadeigbe3@gmail.com>
2023-03-27 11:35:59 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">Adeboss Code</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
